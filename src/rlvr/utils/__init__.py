0938699aa14315442e05541a577121b39ce7c1d0
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-09-08 21:08:11 +0300

    fix

--- src/rlvr/utils/__init__.py
+++ src/rlvr/utils/__init__.py
@@ -1,15 +1,5 @@
 """RLVR utility functions."""
 
-from .logging_helpers import (
-    format_conversation,
-    extract_trajectory_stats,
-    format_input_data,
-    format_reward_components
-)
+from .logging_helpers import extract_trajectory_stats, format_conversation, format_reward_components
 
-__all__ = [
-    "format_conversation",
-    "extract_trajectory_stats",
-    "format_input_data",
-    "format_reward_components"
-]
+__all__ = ["format_conversation", "extract_trajectory_stats", "format_reward_components"]
